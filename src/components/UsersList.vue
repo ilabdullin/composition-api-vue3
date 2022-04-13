b7d31a148ffc06f5922d0a010f82365cc340eef1
ilabdullin <ilnurabdullin578@gmail.com>
2022-04-13 17:32:57 +0500

    first commit

---
+++ src/components/UsersList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="card">
+        <ul class="list">
+            <div class="list-item" v-for="user in users" :key="user.id">
+                {{ user }}
+            </div>
+        </ul>
+    </div>
+</template>
+
+<script>
+import { useUsers } from '../use/users';
+
+export default {
+    async setup() {
+        const { users } = await useUsers();
+        return { users };
+    },
+};
+</script>
